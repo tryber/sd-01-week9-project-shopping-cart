aae01f3726cce0e4d607245191f3d1093d855c2e
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2019-11-06 12:10:55 -0300

    fix some codeclimate issues

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 window.onload = function () {};
 
-function createProductItemElement({ sku, name, image }) {
-  let section = document.createElement("section");
-  section.className = "item";
-
-  section.appendChild(createCustomElement("span", "item__sku", sku));
-  section.appendChild(createCustomElement("span", "item__title", name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement("button", "item__add", "Adicionar ao carrinho!"));
-
-  return section;
-}
-
-function createProductImageElement(image_source) {
-  let img = document.createElement("img");
-  img.className = "item__image";
-  img.src = image_source;
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
   return img;
 }
 
 function createCustomElement(element, className, innerText) {
-  let e = document.createElement(element);
+  const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
 
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+  return section;
+}
+
 function getSkuFromProductItem(item) {
-  return item.querySelector("span.item__sku").innerText;
+  return item.querySelector('span.item__sku').innerText;
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
-  let li = document.createElement("li");
-  li.className = "cart__item";
+  let li = document.createElement('li');
+  li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener("click", cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
