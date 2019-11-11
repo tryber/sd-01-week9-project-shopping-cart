baeb8343de090a604a65ea378575d5c18217f536
Mateus Talles <mateustalles@gmail.com>
2019-11-11 15:25:15 -0300

    Ex. 3 done

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 window.onload = function onload() {
-  const nameInput = document.querySelector('.input-name')
-  nameInput.addEventListener('change', (event) => sessionStorage.setItem('name', event.target.value))
+  function storeName () {
+    const nameInput = document.querySelector('.input-name')
+    nameInput.addEventListener('change', (event) => sessionStorage.setItem('name', event.target.value))
+  }
+
+  function termsAgreementCookies() {
+    const termsAgreementCheckbox = document.querySelector('.input-terms')
+    termsAgreementCheckbox.addEventListener('change', () => {
+      if (event.target.checked) {
+        setCookie('terms_agreement', true, 365)
+      } else {
+        setCookie('terms_agreement', false, 365)
+      }
+    })
+  }
+
+  storeName()
+  termsAgreementCookies()
 };
 
 function getAPI () {
   return localStorage.getItem('API_KEY')
 }
 
-
+function setCookie(name, value, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  const expires = d.toUTCString()
+  document.cookie = `${name}=${value};expires=${expires};path=/`
+}
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
