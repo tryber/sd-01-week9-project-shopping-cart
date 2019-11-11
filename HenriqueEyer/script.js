8790ade312778a331dc4fe3aabe64df704d28c4c
Henrique Eyer <henriqueeyer@outlook.com>
2019-11-11 15:55:13 -0300

    Feita fetch para pegar os valores da api

--- HenriqueEyer/script.js
+++ HenriqueEyer/script.js
@@ -1,58 +1,77 @@
 window.onload = function onload() {
 
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
+  function createProductImageElement(imageSource) {
+    const img = document.createElement('img');
+    img.className = 'item__image';
+    img.src = imageSource;
+    return img;
+  }
 
-function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-}
+  function createCustomElement(element, className, innerText) {
+    const e = document.createElement(element);
+    e.className = className;
+    e.innerText = innerText;
+    return e;
+  }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
+  function createProductItemElement({ sku, name, image }) {
+    const section = document.createElement('section');
+    section.className = 'item';
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+    section.appendChild(createCustomElement('span', 'item__sku', sku));
+    section.appendChild(createCustomElement('span', 'item__title', name));
+    section.appendChild(createProductImageElement(image));
+    section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-  return section;
-}
+    return section;
+  }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+  function getSkuFromProductItem(item) {
+    return item.querySelector('span.item__sku').innerText;
+  }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+  function cartItemClickListener(event) {
+    // coloque seu código aqui
+  }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+  function createCartItemElement({ sku, name, salePrice }) {
+    const li = document.createElement('li');
+    li.className = 'cart__item';
+    li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+    li.addEventListener('click', cartItemClickListener);
+    return li;
+  }
 
-const getApi=()=>localStorage.getItem('api')
+  const getApi = () => localStorage.getItem('api')
 
-const setKeyStorage=(nameKey,value)=>sessionStorage[nameKey]=value;
+  const setKeyStorage = (nameKey, value) => sessionStorage[nameKey] = value;
 
-const getNameUser=()=>document.querySelector('.input-name').value;
+  const getNameUser = () => document.querySelector('.input-name').value;
 
-function saveUser(){
-  setKeyStorage('user',getNameUser());
-}
+  function saveUser() {
+    setKeyStorage('user', getNameUser());
+  }
 
-document.querySelector('.input-name').addEventListener('change', function(){
-  saveUser();
-})
+  document.querySelector('.input-name').addEventListener('change', function () {
+    saveUser();
+  })
+
+  const API_URL = `https://api.bestbuy.com/v1/products(releaseDate>today&categoryPath.id in(cat02001))?apiKey=${getApi()}&format=json&pageSize=30&show=sku,name,image,customerTopRated&sort=bestSellingRank`;
+  console.log(API_URL);
+  catchDados()
+    .then(response => {
+      console.log('OK')
+    })
+    .catch(error => {
+      console.log('error');
+      console.log(error);
+    });
+
+  async function catchDados() {
+    const response = await fetch(API_URL, {
+      headers: { Accept: "application/json" }
+    })
+    const json = await response.json();
+    console.log(json)
+  }
 }
