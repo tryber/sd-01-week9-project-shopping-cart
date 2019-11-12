aef177e4af720507000c28cf65cf28c3aa016157
Guilherme Luiz <guiiluiz44@gmail.com>
2019-11-12 18:14:56 -0300

    cria funcao para adicionar item ao carrinho

--- guiiluiz/script.js
+++ guiiluiz/script.js
@@ -6,8 +6,9 @@ script.crossOrigin = "anonymous"
 document.getElementsByTagName('head')[0].appendChild(script);
 
 window.onload = function onload() {
-  const endPoint = `https://api.bestbuy.com/v1/products(releaseDate>today&
-categoryPath.id in(cat02001))?apiKey=${localStorage.apiKey}&format=json&
+  const apiKey = localStorage.apiKey
+  const productsEndPoint = `https://api.bestbuy.com/v1/products(releaseDate>today&
+categoryPath.id in(cat02001))?apiKey=${apiKey}&format=json&
 pageSize=30&show=sku,name,image,customerTopRated&sort=bestSellingRank`
   const inputName = document.querySelector(".input-name")
   const itemsSection = document.querySelector(".items")
@@ -16,9 +17,19 @@ pageSize=30&show=sku,name,image,customerTopRated&sort=bestSellingRank`
     sessionStorage.name = inputName.value
   })
 
-  $.getJSON(endPoint, (data) => {
-  data.products.forEach(product => itemsSection.appendChild(createProductItemElement(product)))
+  $.getJSON(productsEndPoint, (data) => {
+  data.products.forEach((product,index) => {
+    let productSku = product.sku
+    itemsSection.appendChild(createProductItemElement(product))
+    document.getElementsByClassName("item__add")[index].addEventListener("click", () => {
+      $.getJSON(`https://api.bestbuy.com/v1/products(sku=${productSku})?apiKey=${apiKey}&sort=sku.asc&show=sku,name,salePrice&format=json`, (selected) => {
+        document.querySelector(".cart__items").appendChild(createCartItemElement(selected.products[0]))
+        })
+      })
+    index++
+    })
   })
+
 };
 
 function createProductImageElement(imageSource) {
