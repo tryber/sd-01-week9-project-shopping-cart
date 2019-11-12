9bc4c9a3f6a878ffc17888612645713ed923a1ea
Guilherme Luiz <guiiluiz44@gmail.com>
2019-11-12 19:49:18 -0300

    resolve erros codeclimate

--- guiiluiz/script.js
+++ guiiluiz/script.js
@@ -6,28 +6,28 @@ script.crossOrigin = 'anonymous';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 window.onload = function onload() {
-  const apiKey = localStorage.apiKey
+  const apiKey = localStorage.apiKey;
   const productsEndPoint = `https://api.bestbuy.com/v1/products(releaseDate>today&
 categoryPath.id in(cat02001))?apiKey=${apiKey}&format=json&
-pageSize=30&show=sku,name,image,customerTopRated&sort=bestSellingRank`
-  const inputName = document.querySelector(".input-name")
-  const itemsSection = document.querySelector(".items")
+pageSize=30&show=sku,name,image,customerTopRated&sort=bestSellingRank`;
+  const inputName = document.querySelector('.input-name');
+  const itemsSection = document.querySelector('.items');
 
-  inputName.addEventListener("input", () => {
-    sessionStorage.name = inputName.value
+  inputName.addEventListener('input', () => {
+    sessionStorage.name = inputName.value;
   })
 
   $.getJSON(productsEndPoint, (data) => {
   data.products.forEach((product,index) => {
-    itemsSection.appendChild(createProductItemElement(product))
-    document.getElementsByClassName("item__add")[index].addEventListener('click', () => {
+    itemsSection.appendChild(createProductItemElement(product));
+    document.getElementsByClassName('item__add')[index].addEventListener('click', () => {
       if (localStorage.shopList == undefined) {
-        localStorage.shopList = JSON.stringify(product.sku)
+        localStorage.shopList = JSON.stringify(product.sku);
       }else {
-        localStorage.shopList += JSON.stringify(product.sku)
+        localStorage.shopList += JSON.stringify(product.sku);
       }
       $.getJSON(`https://api.bestbuy.com/v1/products(sku=${product.sku})?apiKey=${apiKey}&sort=sku.asc&show=sku,name,salePrice&format=json`, (selected) => {
-        document.querySelector(".cart__items").appendChild(createCartItemElement(selected.products[0]))
+        document.querySelector(".cart__items").appendChild(createCartItemElement(selected.products[0]));
         })
       })
     index++
