acce85daad1c836e4effd5d08ca15fcf8d657b18
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-11-13 01:24:17 -0200

    Last Question Trouble

--- bolivar/script.js
+++ bolivar/script.js
@@ -3,6 +3,8 @@ window.onload = function onload() {
         restoreValues();
         storeInput();
         storeCheckbox();
+        displayFunctions();
+        displayList();
     } else {
         console.log("No web storage support")
     }
@@ -39,7 +41,14 @@ function getSkuFromProductItem(item) {
 }
 
 function cartItemClickListener(event) {
-    // coloque seu código aqui
+    const count = 0;
+    document.getElementsByClassName("cart__items")[0].removeChild(event.target);
+    Object.keys(localStorage).forEach((key) => {
+        if (localStorage.getItem(key) == event.target.outerHTML && count == 0) {
+            localStorage.removeItem(key)
+            count += 1
+        }
+    })
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -48,10 +57,6 @@ function createCartItemElement({ sku, name, salePrice }) {
     li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
     li.addEventListener('click', cartItemClickListener);
     return li;
-}
-
-function acessAPIKey(ApiKey) {
-    return localStorage.getItem(ApiKey)
 }
 
 function restoreValues() {
@@ -84,3 +89,63 @@ function convertArrayToObject(array) {
         return obj
     }, {});
 };
+
+
+function generateProduct() {
+    return new Promise(
+        (resolve, reject) => {
+            $.getJSON(
+                `https://api.bestbuy.com/v1/products(releaseDate>today&categoryPath.id in(cat02001))?apiKey=${localStorage.getItem('APIkey')}&format=json&pageSize=30&show=sku,name,image,customerTopRated&sort=bestSellingRank`,
+                (data) => {
+                    data.products.forEach((elm_obj) => {
+                        document.getElementsByClassName("items")[0].appendChild(createProductItemElement(elm_obj))
+                    })
+                }
+            )
+        })
+}
+
+function buttonListener() {
+    let buttons = document.getElementsByClassName("item__add");
+    for (i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", event => addShoppingCar(event.target.parentNode.firstElementChild.innerText))
+    }
+}
+
+
+function displayFunctions() {
+    generateProduct();
+    setTimeout(() => {
+        buttonListener()
+    }, 1000);
+}
+
+function addShoppingCar(SKU) {
+    return new Promise(
+        (resolve, reject) => {
+            $.getJSON(
+                `https://api.bestbuy.com/v1/products(sku=${SKU})?apiKey=${localStorage.getItem('APIkey')}&sort=sku.asc&show=sku,name,salePrice&format=json`,
+                (data) => {
+                    document.getElementsByClassName("cart__items")[0].appendChild(createCartItemElement(data.products[0]))
+                    localStorage.setItem(i, createCartItemElement(data.products[0]).outerHTML);
+
+                    createCartItemElement(data.products[0]).addEventListener("click", cartItemClickListener)
+                }
+            )
+        })
+}
+
+function displayList() {
+    const newArray = new Array;
+    for (i = 0; i < Object.keys(localStorage).length; i++) {
+        if (localStorage.getItem(Object.keys(localStorage)[i]).charAt(0) == "<") {
+            newArray.push(Object.keys(localStorage)[i])
+            newArray.sort((a, b) => a - b)
+        }
+    }
+    newArray.forEach((key) => document.getElementsByClassName("cart__items")[0].innerHTML += localStorage.getItem(key))
+    for (i = 0; i < document.getElementsByClassName("cart__item").length; i++) {
+        document.getElementsByClassName("cart__item")[i].addEventListener("click", cartItemClickListener)
+            // console.log(document.getElementsByClassName("cart__item")[i])
+    }
+}
