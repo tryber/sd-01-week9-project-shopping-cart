84cbc4005211c183fdf66914dad6d50ede6dbadf
Douglas Eduardo <douglaseduardo220483@gmail.com>
2019-11-13 12:02:30 -0300

    Colocando ponto e virgula

--- douglaseduardo/script.js
+++ douglaseduardo/script.js
@@ -1,63 +1,71 @@
-window.onload = function onload() {}
+window.onload = function onload() {};
+const limparCarrinho = () => {
+  localStorage.clear();
+  const pai = document.getElementsByClassName('cart__items');
+  const filhos = document.getElementsByClassName('cart__item');
+  for (let i = 0; i < filhos.length; i++) {
+    pai.removeChild(pai.childNodes[i])
+  };
+};
 
 function createProductImageElement(imageSource) {
-  const img = document.createElement('img')
-  img.className = 'item__image'
-  img.src = imageSource
-  return img
-}
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+};
 
 function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element)
-  e.className = className
-  e.innerText = innerText
-  return e
-}
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+};
 
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section')
-  section.className = 'item'
-  section.appendChild(createCustomElement('span', 'item__sku', sku))
-  section.appendChild(createCustomElement('span', 'item__title', name))
-  section.appendChild(createProductImageElement(image))
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
-  return section
-}
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  return section;
+};
 
 function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText
+  return item.querySelector('span.item__sku').innerText;
 }
 
 function cartItemClickListener(event) {
-  const chave = Object.keys(localStorage).find(item => localStorage[item] === event.target.innerText.substring(5, 13))
-  localStorage.removeItem(chave)
-  event.target.remove()
-}
+  const chave = Object.keys(localStorage).find(item => localStorage[item] === event.target.innerText.substring(5, 13));
+  localStorage.removeItem(chave);
+  event.target.remove();
+};
 
 function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li')
-  li.className = 'cart__item'
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`
-  li.addEventListener('click', cartItemClickListener)
-  return li
-}
-const pgClss = classe => document.querySelector(`.${classe}`)
-const api = () => localStorage.getItem('chave_API')
-const apiKeyEx5 = (SKU) => `https://api.bestbuy.com/v1/products(sku=${SKU})?apiKey=${api()}&sort=sku.asc&show=sku,name,salePrice&format=json`
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+};
+const pgClss = classe => document.querySelector(`.${classe}`);
+
+const api = () => localStorage.getItem('chave_API');
+
+const apiKeyEx5 = (SKU) => `https://api.bestbuy.com/v1/products(sku=${SKU})?apiKey=${api()}&sort=sku.asc&show=sku,name,salePrice&format=json`;
 
 Object.keys(localStorage).forEach(chave => {
-  fetch(apiKeyEx5(Number(localStorage.getItem(chave))), {
-      headers: { Accept: 'application/json' }
-    })
+  fetch(apiKeyEx5(Number(localStorage.getItem(chave))), { headers: { Accept: 'application/json' } })
     .then(response => response.json())
-    .then(array => criarListaElemento(array.products))
+    .then(array => criarListaElemento(array.products));
+});
 
-})
+let contador = 0;
 
-let contador = 0
-const criarListaElemento = (respostaJson) => {
-  respostaJson.forEach(element => pgClss('cart__items').appendChild(createCartItemElement(element)))
-}
+const criarListaElemento = respostaJson => {
+  respostaJson.forEach(element => pgClss('cart__items').appendChild(createCartItemElement(element)));
+};
 
 const criarElemento = (valoresParaCriar) => {
   valoresParaCriar.forEach(element => {
@@ -69,39 +77,36 @@ const criarElemento = (valoresParaCriar) => {
         .then(array => criarListaElemento(array.products))
         .then(() => contador++)
         .then(() => modificarLista())
-    })
-  })
-}
+    });
+  });
+};
 
 function criadorKey() {
-  let listaUniversal = ''
-  const carrinho = document.getElementsByClassName('cart__item')
+  let listaUniversal = '';
+  const carrinho = document.getElementsByClassName('cart__item');
   for (let i of carrinho) {
     listaUniversal = `SKU_${i.innerText.substring(5, 13)}_Num_${contador}`
-  }
-  return listaUniversal
-}
+  };
+  return listaUniversal;
+};
 
 function modificarLista() {
-  const olOrdenado = document.getElementsByClassName('cart__item')
+  const olOrdenado = document.getElementsByClassName('cart__item');
   for (let i = 0; i < olOrdenado.length; i++) {
     localStorage[criadorKey()] = olOrdenado[i].innerText.substring(5, 13)
-  }
-  return localStorage
-}
+  };
+  return localStorage;
+};
 
 const usarAPI = () => {
-  const endPoint = () => `https://api.bestbuy.com/v1/products(releaseDate>today&categoryPath.id in(cat02001))?apiKey=${api()}&format=json&pageSize=30&show=sku,name,image,customerTopRated&sort=bestSellingRank`
-  fetch(endPoint(), {
-      headers: { Accept: 'application/json' }
-    })
+  const endPoint = () => `https://api.bestbuy.com/v1/products(releaseDate>today&categoryPath.id in(cat02001))?apiKey=${api()}&format=json&pageSize=30&show=sku,name,image,customerTopRated&sort=bestSellingRank`;
+  fetch(endPoint(), { headers: { Accept: 'application/json' } })
     .then(response => response.json())
     .then(array => criarElemento(array.products))
+};
 
-}
-
-usarAPI()
+usarAPI();
 
 pgClss('input-name').addEventListener('blur', () => {
   return localStorage.nome = inputName.value
-})
+});
