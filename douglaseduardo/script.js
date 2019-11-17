555a1547d0cb628af93296d23151a11b1e575ee7
Douglas Eduardo <douglaseduardo220483@gmail.com>
2019-11-17 11:58:18 -0300

    Tentadno passr o documento

--- douglaseduardo/script.js
+++ douglaseduardo/script.js
@@ -1,8 +1,8 @@
-window.onload = function onload() {};
+global.window.onload = function onload() {};
 
-const pgClss = classe => document.querySelector(`.${classe}`);
+const pgClss = classe => global.document.querySelector(`.${classe}`);
 
-const api = () => localStorage.getItem("chave_API");
+const api = () => global.localStorage.getItem('chave_API');
 
 const apiKeyEx5 = SKU =>
   `https://api.bestbuy.com/v1/products(sku=${SKU})?apiKey=${api()}&sort=sku.asc&show=sku,name,salePrice&format=json`;
@@ -11,32 +11,33 @@ let contador = 0;
 
 function criadorKey() {
   let listaUniversal = '';
-  const carrinho = document.getElementsByClassName('cart__item');
-  carrinho.map(
-    select =>
-      (listaUniversal = `SKU_${select.innerText.substring(5, 13)}_Num_${contador}`));
+  const carrinho = global.document.getElementsByClassName('cart__item');
+  carrinho.map(select => {
+    listaUniversal = `SKU_${select.innerText.substring(5, 13)}_Num_${contador}`;
+    return listaUniversal;
+  });
 
   return listaUniversal;
 }
 
 function modificarLista() {
-  const olOrdenado = document.getElementsByClassName('cart__item');
+  const olOrdenado = global.document.getElementsByClassName('cart__item');
   for (let i = 0; i < olOrdenado.length; i += 1) {
-    localStorage[criadorKey()] = olOrdenado[i].innerText.substring(5, 13);
+    global.localStorage[criadorKey()] = olOrdenado[i].innerText.substring(5, 13);
   }
-  return localStorage;
+  return global.localStorage;
 }
 
 function cartItemClickListener(event) {
-  const chave = Object.keys(localStorage).find(
-    item => localStorage[item] === event.target.innerText.substring(5, 13)
+  const chave = Object.keys(global.localStorage).find(
+    item => global.localStorage[item] === event.target.innerText.substring(5, 13)
   );
-  localStorage.removeItem(chave);
+  global.localStorage.removeItem(chave);
   event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
+  const li = global.document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
@@ -45,63 +46,63 @@ function createCartItemElement({ sku, name, salePrice }) {
 
 function criarListaElemento(respostaJson) {
   respostaJson.forEach(element =>
-    pgClss('cart__items').appendChild(createCartItemElement(element)),
+    pgClss('cart__items').appendChild(createCartItemElement(element))
   );
 }
 
-document.querySelector('.limparCarrinho').addEventListener('click', () => {
-  const pai = document.querySelectorAll('.cart__item');
+global.document.querySelector('.limparCarrinho').addEventListener('click', () => {
+  const pai = global.document.querySelectorAll('.cart__item');
   pai.forEach(item => item.remove());
-  Object.keys(localStorage).forEach((chave) => {
-    if (chave !== 'chave_API') localStorage.removeItem(chave);
+  Object.keys(global.localStorage).forEach(chave => {
+    if (chave !== 'chave_API') global.localStorage.removeItem(chave);
   });
 });
 
 function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
+  const img = global.document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
 
 function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
+  const e = global.document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
 
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
+  const section = global.document.createElement('section');
   section.className = 'item';
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  section.appendChild(
-    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
-  );
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   return section;
 }
 
-Object.keys(localStorage).forEach((chave) => {
-  fetch(apiKeyEx5(Number(localStorage.getItem(chave))), {
-    headers: { Accept: 'application/json' },
+Object.keys(global.localStorage).forEach(chave => {
+  fetch(apiKeyEx5(Number(global.localStorage.getItem(chave))), {
+    headers: { Accept: 'application/json' }
   })
     .then(response => response.json())
     .then(array => criarListaElemento(array.products));
 });
 
-const criarElemento = (valoresParaCriar) => {
-  valoresParaCriar.forEach((element) => {
+const criarElemento = valoresParaCriar => {
+  valoresParaCriar.forEach(element => {
     const filho = createProductItemElement(element);
     pgClss('items').appendChild(filho);
     filho.lastChild.addEventListener('click', () => {
       fetch(apiKeyEx5(filho.firstChild.innerHTML), {
-        headers: { Accept: 'application/json' },
+        headers: { Accept: 'application/json' }
       })
         .then(response => response.json())
         .then(array => criarListaElemento(array.products))
-        .then(() => (contador += 1))
+        .then(() => {
+          contador += 1;
+        })
         .then(() => modificarLista());
     });
   });
@@ -117,6 +118,6 @@ const usarAPI = () => {
 
 usarAPI();
 
-pgClss('input-name').addEventListener(
-  'blur',
-  () => (localStorage.nome = inputName.value));
+pgClss('input-name').addEventListener('blur', () => {
+  global.localStorage.nome = pgClss('input-name').value;
+});
