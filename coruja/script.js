6bc87823e21417cba1acbc2a42f7efb0e6cd65b4
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-12-31 19:06:29 -0300

    item bonus loading finalizado

--- coruja/script.js
+++ coruja/script.js
@@ -1,3 +1,17 @@
+
+function addLoading() {
+  newSpan = document.createElement('span');
+  newSpan.className = 'loading';
+  newSpan.innerHTML = 'Loading...';
+  document.getElementsByClassName('top-bar')[0].appendChild(newSpan);
+}
+function removeLoading() {
+  const loading = document.getElementsByClassName('loading')[0];
+  if (loading) {
+    loading.remove();
+  }
+} 
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -22,18 +36,6 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-// function generateLocalStoragePosition() {
-//   let localStorageLength = localStorage.length;
-//   while (localStorage[localStorageLength]) {
-//     localStorageLength += 1;
-//   }
-//   return localStorageLength;
-// }
-
-// const setKeyStorageCar = ({ sku }) => {
-//   localStorage[generateLocalStoragePosition()] = sku;
-// };
-
 function API_KEY() {
   return localStorage.getItem('API_KEY');
 }
@@ -45,21 +47,8 @@ function saveNameInPage() {
   });
 }
 
-function cartItemClickListener(event) {
+function cartItemClickListener(event, value) {
   event.target.remove();
-}
-
-function clearCarItem() {
-  const button = document.getElementsByTagName('button')[0];
-  button.addEventListener('click', () => {
-    const elementOfCarItem = document.getElementsByClassName('cart__items')[0];
-    while(elementOfCarItem.firstChild) {
-      elementOfCarItem.firstChild.remove();
-    }
-    localStorage.removeItem("comments");
-    carTotal = 0;
-    paragrath.innerText = "0";
-  });
 }
 
 let carTotal = 0;
@@ -70,40 +59,16 @@ function valueOfProduts(salePrice) {
   return paragrath;
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  saveList(sku, name, salePrice);
-  return li;
-}
-
-
-function listOfElementsAtpage() {
-  const valueCar = document.getElementById("value-car");
-  const elementosNoHtml = document.querySelector('.items');
-  const skuNamePrice = document.querySelector('.cart__items');
-  const salePriceCar = document.getElementsByClassName('cart__title')[0];
-  const API_URL = `https://api.bestbuy.com/v1/products(releaseDate>today&categoryPath.id in(cat02001))?apiKey=${API_KEY()}&format=json&pageSize=30&show=sku,name,image,customerTopRated&sort=bestSellingRank`;
-  fetch(API_URL)
-  .then(response => response.json())
-  .then((data) => {
-    data.products.forEach((element) => {
-      const child = createProductItemElement(element);
-      elementosNoHtml.appendChild(child);
-      child.lastChild.addEventListener('click', () => {
-        const API_URL_PRODUCT = `https://api.bestbuy.com/v1/products(sku=${child.firstChild.textContent})?apiKey=${API_KEY()}&sort=sku.asc&show=sku,name,salePrice&format=json`;
-        fetch(API_URL_PRODUCT)
-        .then(response => response.json())
-        .then((dados) => {
-          skuNamePrice.appendChild(createCartItemElement(dados.products[0]));
-          if(valueCar) {valueCar.remove()};
-          salePriceCar.appendChild(valueOfProduts(dados.products[0].salePrice));
-          // setKeyStorageCar(dados.products[0]);
-        });
-      });
-    });
+function clearCarItem() {
+  const button = document.getElementsByTagName('button')[0];
+  button.addEventListener('click', () => {
+    const elementOfCarItem = document.getElementsByClassName('cart__items')[0];
+    while (elementOfCarItem.firstChild) {
+      elementOfCarItem.firstChild.remove();
+    }
+    localStorage.removeItem('comments');
+    carTotal = 0;
+    paragrath.innerText = '0';
   });
 }
 
@@ -122,19 +87,56 @@ function saveList(sku, name, salePrice) {
 
 function showList() {
   const tagOl = document.querySelector('.cart__items');
-  if(localStorage.comments) {
-    let local = JSON.parse(localStorage.comments);
-    for(let index = 0; index < local.length; index++) {
-      const li = document.createElement("li");
+  if (localStorage.comments) {
+    const local = JSON.parse(localStorage.comments);
+    for (let index = 0; index < local.length; index+= 1) {
+      const li = document.createElement('li');
       li.innerHTML = `SKU: ${local[index].sku} | NAME: ${local[index].name} | PRICE: $${local[index].salePrice}`;
       tagOl.appendChild(li);
       li.addEventListener('click', cartItemClickListener);
-      clearCarItem()
+      clearCarItem();
     }
   }
 }
 
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  saveList(sku, name, salePrice);
+  return li;
+}
+
+function listOfElementsAtpage() {
+  const valueCar = document.getElementById('value-car');
+  const elementosNoHtml = document.querySelector('.items');
+  const skuNamePrice = document.querySelector('.cart__items');
+  const salePriceCar = document.getElementsByClassName('cart__title')[0];
+  const API_URL = `https://api.bestbuy.com/v1/products(releaseDate>today&categoryPath.id in(cat02001))?apiKey=${API_KEY()}&format=json&pageSize=30&show=sku,name,image,customerTopRated&sort=bestSellingRank`;
+  fetch(API_URL)
+  .then(response => response.json())
+  .then((data) => {
+    data.products.forEach((element) => {
+      const child = createProductItemElement(element);
+      elementosNoHtml.appendChild(child);
+      removeLoading();
+      child.lastChild.addEventListener('click', () => {
+        const API_URL_PRODUCT = `https://api.bestbuy.com/v1/products(sku=${child.firstChild.textContent})?apiKey=${API_KEY()}&sort=sku.asc&show=sku,name,salePrice&format=json`;
+        fetch(API_URL_PRODUCT)
+        .then(response => response.json())
+        .then((dados) => {
+          skuNamePrice.appendChild(createCartItemElement(dados.products[0]));
+          if (valueCar) { valueCar.remove(); }
+          salePriceCar.appendChild(valueOfProduts(dados.products[0].salePrice));
+        });
+      });
+    });
+  });
+}
+
 window.onload = function onload() {
+  addLoading();
   API_KEY();
   saveNameInPage();
   listOfElementsAtpage();
